b68dfd1d1569bf55db50995174ef06bf3610785d
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-11-07 22:56:10 +0600

    done

--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -13,7 +13,7 @@ const Appointments = ({ date }) => {
     const [appointments, setAppointments] = useState([])
 
     useEffect(() => {
-        const url = `https://doctors-portal-21k-server.herokuapp.com/appointments?email=${user.email}&date=${date}`
+        const url = `https://doctors-portal-21k-server.herokuapp.com/appointments?email=${user.email}&date=${date.toLocaleDateString()}`
         fetch(url, {
             headers: {
                 'authorization': `Bearer ${token}`,
